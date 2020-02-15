74c5870ea677fb971f980e852058e8d6f3913233
Vladislav <evigertsik@gmail.com>
2020-02-15 22:16:18 +0300

    Do tasks 1-6

---
+++ task/04-arrays-tasks.js
@@ -0,0 +1,664 @@
+"use strict";
+
+/*********************************************************************************************
+ *                                                                                           *
+ * Plese read the following tutorial before implementing tasks:                              *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array    *
+ *                                                                                           *
+ * NOTE : Please do not use loops! All tasks can be implmeneted using standard Array methods *
+ *                                                                                           *
+ *********************************************************************************************/
+
+/**
+ * Returns an index of the specified element in array or -1 if element is not found
+ *
+ * @param {array} arr
+ * @param {any} value
+ * @return {number}
+ *
+ * @example
+ *    ['Ace', 10, true], 10    => 1
+ *    ['Array', 'Number', 'string'], 'Date'    => -1
+ *    [0, 1, 2, 3, 4, 5], 5    => 5
+ */
+function findElement(arr, value) {
+    return arr.indexOf(value) ? arr.indexOf(value) : -1;
+}
+
+/**
+ * Generates an array of odd numbers of the specified length
+ *
+ * @param {number} len
+ * @return {array}
+ *
+ * @example
+ *    1 => [ 1 ]
+ *    2 => [ 1, 3 ]
+ *    5 => [ 1, 3, 5, 7, 9 ]
+ */
+function generateOdds(len) {
+    var arr = [];
+    var arrItem = 1;
+    var i = 0;
+    do {
+        arr.push(arrItem);
+        arrItem += 2;
+        i++;
+    } while (i < len);
+    return arr;
+}
+
+/**
+ * Returns the doubled array - elements of the specified array are repeated twice using original order
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    ['Ace', 10, true]  => ['Ace', 10, true,   'Ace', 10, true]
+ *    [0, 1, 2, 3, 4, 5] => [0, 1, 2, 3, 4, 5,   0, 1, 2, 3, 4, 5]
+ *    [] => []
+ */
+function doubleArray(arr) {
+    return arr.concat(arr);
+}
+
+/**
+ * Returns an array of positive numbers from the specified array in original order
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    [ 0, 1, 2, 3, 4, 5 ] => [ 1, 2, 3, 4, 5 ]
+ *    [-1, 2, -5, -4, 0] => [ 2 ]
+ *    [] => []
+ */
+function getArrayOfPositives(arr) {
+    return arr.filter(arrItem => {
+        return arrItem > 0;
+    });
+}
+
+/**
+ * Returns the array with strings only in the specified array (in original order)
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    [ 0, 1, 'cat', 3, true, 'dog' ] => [ 'cat', 'dog' ]
+ *    [ 1, 2, 3, 4, 5 ] => []
+ *    [ 'cat, 'dog', 'raccon' ] => [ 'cat', 'dog', 'racoon' ]
+ */
+function getArrayOfStrings(arr) {
+    return arr.filter((arrItem) => {
+        return typeof arrItem == 'string'
+    });
+}
+console.log(getArrayOfStrings([0, 1, 'cat', 3, true, 'dog']));
+console.log(getArrayOfStrings([1, 2, 3, 4, 5]));
+
+
+/**
+ * Removes falsy values from the specified array
+ * Falsy values: false, null, 0, "", undefined, and NaN.
+ * (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean#Description)
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    [ 0, false, 'cat', NaN, true, '' ] => [ 'cat', true ]
+ *    [ 1, 2, 3, 4, 5, 'false' ]         => [ 1, 2, 3, 4, 5, 'false' ]
+ *    [ false, 0, NaN, '', undefined ]   => [ ]
+ */
+function removeFalsyValues(arr) {
+    return arr.filter(Boolean);
+}
+
+/**
+ * Returns the array of useprcase strings from the specified array
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    [ 'permanent-internship', 'glutinous-shriek', 'multiplicative-elevation' ] => [ 'PERMANENT-INTERNSHIP', 'GLUTINOUS-SHRIEK', 'MULTIPLICATIVE-ELEVATION' ]
+ *    [ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]  => [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ]
+ */
+function getUpperCaseStrings(arr) {
+    return arr.map(function (x) {
+        return x.toUpperCase();
+    });
+}
+
+/**
+ * Returns the array of string lengths from the specified string array.
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    [ '', 'a', 'bc', 'def', 'ghij' ]  => [ 0, 1, 2, 3, 4 ]
+ *    [ 'angular', 'react', 'ember' ] => [ 7, 5, 5 ]
+ */
+function getStringsLength(arr) {
+    var arr2 = [];
+    arr.map(function (x) {
+        arr2.push(x.length);
+    });
+    return arr2;
+}
+
+/**
+ * Inserts the item into specified array at specified index
+ *
+ * @param {array} arr
+ * @param {any} item
+ * @param {number} index
+ *
+ * @example
+ *    [ 1, 3, 4, 5 ], 2, 1  => [ 1, 2, 3, 4, 5 ]
+ *    [ 1, 'b', 'c'], 0, 'x'  => [ 'x', 1, 'b', 'c' ]
+ */
+function insertItem(arr, item, index) {
+    return arr.splice(index, 0, item);
+}
+
+/**
+ * Returns the n first items of the specified array
+ *
+ * @param {array} arr
+ * @param {number} n
+ *
+ * @example
+ *    [ 1, 3, 4, 5 ], 2  => [ 1, 2 ]
+ *    [ 'a', 'b', 'c', 'd'], 3  => [ 'a', 'b', 'c' ]
+ */
+function getHead(arr, n) {
+    return arr.slice(0, n);
+}
+
+/**
+ * Returns the n last items of the specified array
+ *
+ * @param {array} arr
+ * @param {number} n
+ *
+ * @example
+ *    [ 1, 3, 4, 5 ], 2  => [ 4, 5 ]
+ *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
+ */
+function getTail(arr, n) {
+    return arr.slice(-n);
+}
+console.log(getTail([1, 3, 4, 5], 2));
+
+
+/**
+ * Returns CSV represebtation of two-dimentional numeric array.
+ * https://en.wikipedia.org/wiki/Comma-separated_values
+ *
+ * @param {array} arr
+ * @return {string}
+ *
+ * @example
+ *    [
+ *       [  0, 1, 2, 3, 4 ],
+ *       [ 10,11,12,13,14 ],
+ *       [ 20,21,22,23,24 ],
+ *       [ 30,31,32,33,34 ]
+ *    ]
+ *           =>
+ *     '0,1,2,3,4\n'
+ *    +'10,11,12,13,14\n'
+ *    +'20,21,22,23,24\n'
+ *    +'30,31,32,33,34'
+ */
+function toCsvText(arr) {
+    return arr.join('\n');
+
+    // for (var i = 0; i < arr.length; i++) {
+    //     masStr.add(string.join(arr[i], ","));
+    // }
+    // return String.join(masStr, "\n");
+}
+
+/**
+ * Transforms the numeric array into the according array of squares:
+ *   f(x) = x * x
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *   [ 0, 1, 2, 3, 4, 5 ] => [ 0, 1, 4, 9, 16, 25 ]
+ *   [ 10, 100, -1 ]      => [ 100, 10000, 1 ]
+ */
+function toArrayOfSquares(arr) {
+    return arr.map((arrItem) => Math.pow(arrItem, 2));;
+}
+console.log(toArrayOfSquares([0, 1, 2, 3, 4, 5]));
+
+/**
+ * Transforms the numeric array to the according moving sum array:
+ *     f[n] = x[0] + x[1] + x[2] +...+ x[n]
+ *  or f[n] = f[n-1] + x[n]
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * Example :
+ *   [ 1, 1, 1, 1, 1 ]        => [ 1, 2, 3, 4, 5 ]
+ *   [ 10, -10, 10, -10, 10 ] => [ 10, 0, 10, 0, 10 ]
+ *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
+ *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
+ */
+function getMovingSum(arr) {
+    arr.reduce((accum, elem, i) => arr[i] = accum + elem, 0);
+    return arr;
+}
+
+/**
+ * Returns every second item from the specified array:
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * Example :
+ * [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 2, 4, 6, 8, 10 ]
+ * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
+ * [ "a" ] => []
+ */
+function getSecondItems(arr) {
+    return arr.filter((arrItem, index) => index % 2 === 1);
+}
+
+/**
+ * Propagates every item in sequence its position times
+ * Returns an array that consists of: one first item, two second items, tree third items etc.
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example :
+ *  [] => []
+ *  [ 1 ] => [ 1 ]
+ *  [ 'a', 'b' ] => [ 'a', 'b','b' ]
+ *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
+ *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
+ */
+function propagateItemsByPositionIndex(arr) {
+   const reducer = (accum, elem, index) => accum.concat(Array.from({
+       length: index + 1
+   }, () => elem));
+   return arr.reduce(reducer, []);
+}
+
+/**
+ * Returns the 3 largest numbers from the specified array
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *   [] => []
+ *   [ 1, 2 ] => [ 2, 1 ]
+ *   [ 1, 2, 3 ] => [ 3, 2, 1 ]
+ *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
+ *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
+ */
+function get3TopItems(arr) {
+    return arr.sort((a, b) => a - b).reverse().slice(0, 3);
+};
+
+
+/**
+ * Returns the number of positive numbers from specified array
+ *
+ * @param {array} arr
+ * @return {number}
+ *
+ * @example
+ *   [ ]          => 0
+ *   [ -1, 0, 1 ] => 1
+ *   [ 1, 2, 3]   => 3
+ *   [ null, 1, 'elephant' ] => 1
+ *   [ 1, '2' ] => 1
+ */
+function getPositivesCount(arr) {
+    // return arr.filter((arrItem) => arrItem === item).length;
+    return arr.filter(arrItem => typeof arrItem === 'number' && arrItem > 0).length;
+}
+
+/**
+ * Sorts digit names
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *   [] => []
+ *   [ 'nine','one' ]                 => [ 'one', 'nine' ]
+ *   [ 'one','two','three' ]          => [ 'one','two', 'three' ]
+ *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
+ *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
+ */
+function sortDigitNamesByNumericOrder(arr) {
+    let digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let numbers = arr.map(elem => digits.indexOf(elem)).sort();
+    return numbers.map(elem => digits[elem]);
+}
+
+/**
+ * Returns the sum of all items in the specified array of numbers
+ *
+ * @param {array} arr
+ * @return {number}
+ *
+ * @example
+ *   [] => 0
+ *   [ 1, 2, 3 ]           => 6
+ *   [ -1, 1, -1, 1 ]      => 0
+ *   [ 1, 10, 100, 1000 ]  => 1111
+ */
+function getItemsSum(arr) {
+    return arr.reduce((a, b) => a + b, 0);
+}
+
+/**
+ * Returns the number of all falsy value in the specified array
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *  [] => 0
+ *  [ 1, '', 3 ] => 1
+ *  [ -1, 'false', null, 0 ] => 2
+ *  [ null, undefined, NaN, false, 0, '' ]  => 6
+ */
+function getFalsyValuesCount(arr) {
+    // return arr.filter((arrItem) => arrItem === item).length;
+    return arr.filter(arrItem => !Boolean(arrItem)).length;
+}
+console.log(getFalsyValuesCount([]));
+
+/**
+ * Returns a number of all occurences of the specified item in an array
+ *
+ * @param {array} arr
+ * @param {any} item
+ * @return {number}
+ *
+ * @example
+ *    [ 0, 0, 1, 1, 1, 2 ], 1 => 3
+ *    [ 1, 2, 3, 4, 5 ], 0 => 0
+ *    [ 'a','b','c','c' ], 'c'=> 2
+ *    [ null, undefined, null ], null => 2
+ *    [ true, 0, 1, 'true' ], true => 1
+ */
+function findAllOccurences(arr, item) {
+    return arr.filter((arrItem) => arrItem === item).length;
+}
+console.log(findAllOccurences([null, undefined, null], null));
+
+
+/**
+ * Concatenates all elements from specified array into single string with ',' delimeter
+ *
+ * @param {array} arr
+ * @return {string}
+ *
+ * @example
+ *    [0, false, 'cat', NaN, true, '']  => '0,false,cat,NaN,true,'
+ *    [1, 2, 3, 4, 5]                   => '1,2,3,4,5'
+ *    ['rock', 'paper', 'scissors']     => 'rock,paper,scissors'
+ */
+function toStringList(arr) {
+    return arr.toString();
+}
+
+/**
+ * Sorts the specified array by country name first and city name (if countries are equal) in ascending order.
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    [
+ *      { country: 'Russia',  city: 'Moscow' },
+ *      { country: 'Belarus', city: 'Minsk' },
+ *      { country: 'Poland',  city: 'Warsaw' },
+ *      { country: 'Russia',  city: 'Saint Petersburg' },
+ *      { country: 'Poland',  city: 'Krakow' },
+ *      { country: 'Belarus', city: 'Brest' }
+ *    ]
+ *                      =>
+ *    [
+ *      { country: 'Belarus', city: 'Brest' },
+ *      { country: 'Belarus', city: 'Minsk' },
+ *      { country: 'Poland',  city: 'Krakow' },
+ *      { country: 'Poland',  city: 'Warsaw' },
+ *      { country: 'Russia',  city: 'Moscow' },
+ *      { country: 'Russia',  city: 'Saint Petersburg' }
+ */
+function sortCitiesArray(arr) {
+    const sortF = (a, b) => {
+       return (a.country > b.country) ? 1 : (a.country < b.country) ? -1 :
+              (a.city > b.city) ? 1 : (a.city < b.city) ? -1 : 0;
+    }
+    return arr.sort(sortF);
+
+    const sortFunction = (a, b) => {
+        if (a.country > b.country) {
+            return 1;
+        } else if (a.country < b.country) {
+            return -1;
+        } else if (a.city > b.city) {
+            return 1;
+        } else if (a.city < b.city) {
+            return -1;
+        } else return 0;
+    }
+    return arr.sort(sortFunction);
+}
+
+/**
+ * Creates an indentity matrix of the specified size
+ *
+ * @param {number} n
+ * @return {array}
+ *
+ * @example
+ *     1  => [[1]]
+ *
+ *     2 => [[1,0],
+ *           [0,1]]
+ *
+ *          [[1,0,0,0,0],
+ *           [0,1,0,0,0],
+ *     5 =>  [0,0,1,0,0],
+ *           [0,0,0,1,0],
+ *           [0,0,0,0,1]]
+ */
+function getIdentityMatrix(n) {
+   const len = {
+       length: n
+   };
+   let func = (arr, arrIndex) => Array.from(len, (elem, index) => arrIndex === index ? 1 : 0);
+   return Array.from(len, func);
+}
+
+/**
+ * Creates an array of integers from the specified start to end (inclusive)
+ *
+ * @param {number} start
+ * @param {number} end
+ * @return {array}
+ *
+ * @example
+ *     1, 5  => [ 1, 2, 3, 4, 5 ]
+ *    -2, 2  => [ -2, -1, 0, 1, 2 ]
+ *     0, 100 => [ 0, 1, 2, ..., 100 ]
+ *     3, 3   => [ 3 ]
+ */
+function getIntervalArray(start, end) {
+    return Array.from({
+        length: end - start + 1
+    }, (arrayItem, i) => start + i);
+}
+
+/**
+ * Returns array containing only unique values from the specified array.
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *   [ 1, 2, 3, 3, 2, 1 ] => [ 1, 2, 3 ]
+ *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
+ *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
+ */
+function distinct(arr) {
+    return Array.from(new Set(arr));
+}
+
+/**
+ * Groups elements of the specified array by key.
+ * Returns multimap of keys extracted from array elements via keySelector callback
+ * and values extracted via valueSelector callback.
+ * See: https://en.wikipedia.org/wiki/Multimap
+ *
+ * @param {array} array
+ * @param {Function} keySelector
+ * @param {Function} valueSelector
+ * @return {Map}
+ *
+ * @example
+ *   group([
+ *      { country: 'Belarus', city: 'Brest' },
+ *      { country: 'Russia', city: 'Omsk' },
+ *      { country: 'Russia', city: 'Samara' },
+ *      { country: 'Belarus', city: 'Grodno' },
+ *      { country: 'Belarus', city: 'Minsk' },
+ *      { country: 'Poland', city: 'Lodz' }
+ *     ],
+ *     item => item.country,
+ *     item => item.city
+ *   )
+ *            =>
+ *   Map {
+ *    "Belarus" => ["Brest", "Grodno", "Minsk"],
+ *    "Russia" => ["Omsk", "Samara"],
+ *    "Poland" => ["Lodz"]
+ *   }
+ */
+function group(array, keySelector, valueSelector) {
+    let arrayItemByKey = new Map();
+    array.map((elem) => {
+        let key = keySelector(elem);
+        let value = valueSelector(elem);
+        let collection = arrayItemByKey.get(key);
+        !collection ? arrayItemByKey.set(key, [value]) : collection.push(value);
+    });
+    return arrayItemByKey;
+}
+
+/**
+ * Projects each element of the specified array to a sequence and flattens the resulting sequences into one array.
+ *
+ * @param {array} arr
+ * @param {Function} childrenSelector, a transform function to apply to each element that returns an array of children
+ * @return {array}
+ *
+ * @example
+ *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
+ *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
+ */
+function selectMany(arr, childrenSelector) {
+    return arr.reduce((accum, arrayItem) => accum.concat(childrenSelector(arrayItem)), []);
+}
+
+/**
+ * Returns an element from the multidimentional array by the specified indexes.
+ *
+ * @param {array} arr
+ * @param {array} indexes
+ * @return {any} element from array
+ *
+ * @example
+ *   [[1, 2], [3, 4], [5, 6]], [0,0]  => 1        (arr[0][0])
+ *   ['one','two','three'], [2]       => 'three'  (arr[2])
+ *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
+ */
+function getElementByIndexes(arr, indexes) {
+    if (Array.isArray(arr)) {
+        arr.splice(indexes[0] + 1);
+        arr.splice(0, indexes[0]);
+        indexes.splice(0, 1);
+        return arr.reduce((accum, elem) => accum.concat(getElementByIndexes(elem, indexes)), []);
+    } else {
+        return arr;
+    }
+}
+
+/**
+ * Swaps the head and tail of the specified array:
+ * the head (first half) of array move to the end, the tail (last half) move to the start.
+ * The middle element (if exists) leave on the same position.
+ *
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *   [ 1, 2, 3, 4, 5 ]   =>  [ 4, 5, 3, 1, 2 ]
+ *    \----/   \----/
+ *     head     tail
+ *
+ *   [ 1, 2 ]  => [ 2, 1 ]
+ *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
+ *
+ */
+function swapHeadAndTail(arr) {
+    let oneForEven = arr.length % 2 === 0 ? 1 : 0;
+    let head = arr.splice(0, arr.length / 2);
+    let tail = arr.splice(arr.length / 2 - oneForEven, arr.length);
+    return tail.concat(arr, head);
+}
+
+module.exports = {
+    findElement: findElement,
+    generateOdds: generateOdds,
+    doubleArray: doubleArray,
+    getArrayOfPositives: getArrayOfPositives,
+    getArrayOfStrings: getArrayOfStrings,
+    removeFalsyValues: removeFalsyValues,
+    getUpperCaseStrings: getUpperCaseStrings,
+    getStringsLength: getStringsLength,
+    insertItem: insertItem,
+    getHead: getHead,
+    getTail: getTail,
+    toCsvText: toCsvText,
+    toStringList: toStringList,
+    toArrayOfSquares: toArrayOfSquares,
+    getMovingSum: getMovingSum,
+    getSecondItems: getSecondItems,
+    propagateItemsByPositionIndex: propagateItemsByPositionIndex,
+    get3TopItems: get3TopItems,
+    getPositivesCount: getPositivesCount,
+    sortDigitNamesByNumericOrder: sortDigitNamesByNumericOrder,
+    getItemsSum: getItemsSum,
+    getFalsyValuesCount: getFalsyValuesCount,
+    findAllOccurences: findAllOccurences,
+    sortCitiesArray: sortCitiesArray,
+    getIdentityMatrix: getIdentityMatrix,
+    getIntervalArray: getIntervalArray,
+    distinct: distinct,
+    group: group,
+    selectMany: selectMany,
+    getElementByIndexes: getElementByIndexes,
+    swapHeadAndTail: swapHeadAndTail
+};
